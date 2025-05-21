1724ede090f9a84ff01372e376618bacbc99a83d
Andry Smirnov <Andry.Smirnov@gmail.com>
2025-05-21 15:58:12 +0300

    Обновит pl_components/pl_graphics32ext/pl_graphics32ext.pas

--- pl_components/pl_graphics32ext/pl_graphics32ext.pas
+++ pl_components/pl_graphics32ext/pl_graphics32ext.pas
@@ -1,8 +1,8 @@
-{ This file was automatically created by Typhon IDE. Do not edit!
+{ This file was automatically created by Lazarus. Do not edit!
   This source is only used to compile and install the package.
  }
 
-unit pl_graphics32ext;
+unit pl_Graphics32EXT;
 
 {$warn 5023 off : no warning about unused units}
 interface
@@ -10,14 +10,15 @@ interface
 uses
   AllGraphics32ExtRegister, XGR32_Algebra, XGR32_AniEffects, 
   XGR32_AniGEffetcts, XGR32_Animation, XGR32_BitmapEx, XGR32_Blending, 
-  XGR32_Blendmodes, XGR32_Bmp32Draw, XGR32_Bmp32Func, XGR32_Color, 
-  XGR32_DesktopControl, XGR32_Effects, XGR32_ElasticLayers, XGR32_Emboss, 
-  XGR32_ExtLayers, XGR32_ExtLayers_cursors, XGR32_ExtLayers_Panel, 
-  XGR32_ExtLayers_transform, XGR32_FastFX, XGR32_FilterEx, XGR32_FlareFX, 
-  XGR32_FloodFill, XGR32_GausianBlur, XGR32_Graphics, XGR32_GraphUtils, 
-  XGR32_ImageList, XGR32_ParticleAniEffects, XGR32_ParticleSnow, 
-  XGR32_ParticleStar, XGR32_RasterizerEx, XGR32_RedEye, XGR32_Sprites, 
-  XGR32_StackBlur, XGR32_TransformsWarp, XGR32_Warping, LazarusPackageIntf;
+  XGR32_Blendmodes, XGR32_Bmp32Draw, XGR32_Bmp32Func, XGR32_Clock, 
+  XGR32_Color, XGR32_Controls, XGR32_DesktopControl, XGR32_Effects, 
+  XGR32_ElasticLayers, XGR32_Emboss, XGR32_ExtLayers, XGR32_ExtLayers_cursors, 
+  XGR32_ExtLayers_Panel, XGR32_ExtLayers_transform, XGR32_FastFX, 
+  XGR32_FilterEx, XGR32_FlareFX, XGR32_FloodFill, XGR32_GausianBlur, 
+  XGR32_Graphics, XGR32_GraphUtils, XGR32_ImageList, XGR32_Panel32, 
+  XGR32_ParticleAniEffects, XGR32_ParticleSnow, XGR32_ParticleStar, 
+  XGR32_RasterizerEx, XGR32_RedEye, XGR32_Sprites, XGR32_StackBlur, 
+  XGR32_StdCtrls, XGR32_TransformsWarp, XGR32_Warping, LazarusPackageIntf;
 
 implementation
 
@@ -27,5 +28,5 @@ begin
 end;
 
 initialization
-  RegisterPackage('pl_graphics32ext', @Register);
+  RegisterPackage('pl_Graphics32EXT', @Register);
 end.
